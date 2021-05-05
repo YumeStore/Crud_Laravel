10e1dc6ddc2befbc1bcd890b2cb0be5f86daab63
pedroBaptistaDosSantos <pedro.santos415@etec.sp.gov.br>
2021-05-05 16:08:20 -0300

    Tabela de Cursos exibida na segunda etapap

---
+++ resources/js/aluno.model.js
@@ -0,0 +1,8 @@
+class Aluno {
+    constructor(id, nome, email, cpf){
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+    }
+}
--- resources/views/alunoscurso/create.blade.php
+++ resources/views/alunoscurso/create.blade.php
@@ -10,43 +10,17 @@
     <body>
     @php use Illuminate\Support\Facades\DB;
     $usuarios = DB::table('alunos') -> get();
+    $cursos = DB::table('cursos') -> get();
     @endphp 
         <div class="container">
-
-            <nav aria-label="breadcrumb" id="alunoEscolha">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item">
-                        <a href="#">Escolha do Aluno</a>
-                    </li>
-                </ol>
-            </nav>
-            <nav aria-label="breadcrumb" id="cursoEscolha">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item">
-                        <a href="#">Escolha do Aluno</a>
-                    </li>
-                    <li class="breadcrumb-item">
-                        <a href="#">Escolha o Curso</a>
-                    </li>
-                </ol>
-            </nav>
-            <nav aria-label="breadcrumb" id="bolsaEscolha">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item">
-                        <a href="#">Escolha do Aluno</a>
-                    </li>
-                    <li class="breadcrumb-item">
-                        <a href="#">Escolha o Curso</a>
-                    </li>
-                    <li class="breadcrumb-item active" aria-current="page">Escolha se possui Bolsa</li>
-                </ol>
-            </nav>
-
-            <div class="progress">
-                <div class="progress-bar bg-success" id="progress" role="progressbar" style="width: 0%" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-
-            <div class="content p-1">
+            <div class="row py-4">
+                <div class="progress">
+                    <div class="progress-bar bg-success" id="progress" role="progressbar" style="width: 0%" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>  
+            <form>
+                <!-- Bloco do Aluno -->
+                <div id="escolhaAluno">
                     <div id="aluno">
                         <div class="form-group">
                             <div class="form-group">
@@ -55,153 +29,102 @@
                             </div>
                             <button type="button" id="filtrarAluno" onclick="filtrarAlunoByName()" class="btn btn-info">Filtrar</button>
                         </div>   
-
-                        <form>
-                        <!-- Tabela de aluno cadastrados -->
-                        <div class="form-row">
-                            <table class="table table-dark table-striped">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">Id do Aluno</th>
+                    </div>
+                    <!-- Tabela de aluno cadastrados -->
+                    <div class="form-row">
+                        <table class="table table-dark table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Id do Aluno</th>
                                         <th scope="col">Nome</th>
                                         <th scope="col">Email</th>
                                         <th scope="col"></th>
                                     </tr>
-                                </thead>
-                                <tbody id="tabelaAlunos">
-                                      <!-- Conteudo da tabela que sera apresentado pelo JavaScript -->
-                                      <!-- Conteudo da tabela que sera apresentado pelo JavaScript -->
-                                      <!-- Conteudo da tabela que sera apresentado pelo JavaScript -->
-                                      <!-- Conteudo da tabela que sera apresentado pelo JavaScript -->
-                                </tbody>
+                            </thead>
+                            <tbody id="tabelaAlunos">
+                                <!-- Conteudo da tabela que sera apresentado pelo JavaScript -->
+                            </tbody>
                         </table>
                     </div>
-                    </div>
                     <div class="form-row">
-                        <button id="avancarCurso()" class="btn btn-info">Avançar</button>
+                        <button type="button" onclick="avancarCurso()" class="btn btn-info">Avançar para Curso</button>
                     </div>
                 </div>
-                <div id="curso">
-                    <div class="row">
+                <!-- Bloco do Curso -->
+                <div id="escolhaCurso">
+                    <div class="form-group">
                         <div class="form-group">
-                            <label for="">Curso</label>
-                            <input class="form-control" type="text" name="idCurso">
-                            <br>
+                            <label>Curso:</label>
+                            <input class="form-control col-6" type="text" name="nomeCurso" id="idCursoFiltro">
                         </div>
-                        <button id="avancarBolsa()" class="btn btn-sucess">Avançar</button>
-                    </div>
-                    <div class="row">
-                        <div class="form-group">
-                            <label for="">turno</label>
-                            <input class="form-control" type="text" name="turno">
-                            <br>
-                        </div>
-                    </div>
-                </div>
-                <div id="bolsa">
-                    <div class="row">
-                        <div class="form-group">
-                            <label for="">id da Bolsa</label>
-                            <input class="form-control" type="text" name="idBolsa">
-                            <br>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="form-group">
-                            <label for="">dataTermino</label>
-                            <input class="form-control" type="text" name="observacoes">
-                            <br>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="form-group">
-                            <button class="btn btn-primary" type="button">
-                                Salvar</button>
-                        </div>
-                    </div>
-                </div>
-            </form>
-        </div>
-        <div id="curso">
-                        <div class="form-group">
-                            <div class="form-group">
-                                <label for="">Aluno:</label>
-                                <input class="form-control col-6" type="text" name="nomeAluno" id="idAlunoFiltro">
-                            </div>
-                            <button type="button" id="filtrarCurso" onclick="filtrarCursoByName()" class="btn btn-info">Filtrar</button>
-                        </div>   
+                        <button type="button" id="filtrarCurso" onclick="filtrarCursoByName()" class="btn btn-info">Filtrar</button>
+                    </div>   
 
-                        <form>
-                        <!-- Tabela de aluno cadastrados -->
-                        <div class="form-row">
-                            <table class="table table-dark table-striped">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">Id do Aluno</th>
-                                        <th scope="col">Nome</th>
-                                        <th scope="col">Email</th>
-                                        <th scope="col"></th>
-                                    </tr>
-                                </thead>
-                                <tbody id="tabelaAlunos">
-                                      <!-- Conteudo da tabela que sera apresentado pelo JavaScript -->
-                                      <!-- Conteudo da tabela que sera apresentado pelo JavaScript -->
-                                      <!-- Conteudo da tabela que sera apresentado pelo JavaScript -->
-                                      <!-- Conteudo da tabela que sera apresentado pelo JavaScript -->
-                                </tbody>
+                    <!-- Tabela de Cursos cadastrados -->
+                    <div class="form-row">
+                        <table class="table table-dark table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Id do Curso</th>
+                                    <th scope="col">Nome</th>
+                                    <th scope="col">Imagem</th>
+                                    <th scope="col">Conteudo</th>
+                                    <th scope="col"></th>
+                                </tr>
+                            </thead>
+                            <tbody id="tabelaCursos">
+                                        <!-- Conteudo da tabela que sera apresentado pelo JavaScript -->
+                            </tbody>
                         </table>
                     </div>
-                    </div>
                     <div class="form-row">
-                        <button id="avancarCurso()" class="btn btn-info">Avançar</button>
+                        <button type="button" id="avançarCheckOut()" class="btn btn-info">Avançar</button>
                     </div>
                 </div>
-                <div id="curso">
-                    <div class="row">
+                <div id="checkOut">
+                    <div class="form-group">
+                        <label for="">Aluno</label>
                         <div class="form-group">
-                            <label for="">Curso</label>
-                            <input class="form-control" type="text" name="idCurso">
-                            <br>
+                            <label for="">Id</label>
+                            <input class="form-control col-6" type="text" name="idAluno">
                         </div>
-                        <button id="avancarBolsa()" class="btn btn-sucess">Avançar</button>
-                    </div>
-                    <div class="row">
                         <div class="form-group">
-                            <label for="">turno</label>
-                            <input class="form-control" type="text" name="turno">
-                            <br>
+                            <label for="">Nome</label>
+                            <input class="form-control col-6" type="text" id="nomeAluno">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Email</label>
+                            <input class="form-control col-6" type="text" id="email">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Cpf</label>
+                            <input class="form-control col-6" type="text" id="cpf">
                         </div>
                     </div>
+                    <div class="form-group">
+                        <label for="">Curso</label>
+                        <div class="form-group">
+                            <label for="">Id</label>
+                            <input class="form-control col-6" type="text" name="idCurso" id="idCurso">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Nome</label>
+                            <input class="form-control col-6" type="text" id="nomeCurso">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Imagem</label>
+                            <input class="form-control col-6" type="text" id="conteudo">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Conteudo</label>
+                            <input class="form-control col-6" type="text" id="imagem">
+                        </div>
+                    </div>  
+                    <button type="submit">Submeter</button> 
                 </div>
-                <div id="bolsa">
-                    <div class="row">
-                        <div class="form-group">
-                            <label for="">id da Bolsa</label>
-                            <input class="form-control" type="text" name="idBolsa">
-                            <br>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="form-group">
-                            <label for="">dataTermino</label>
-                            <input class="form-control" type="text" name="observacoes">
-                            <br>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="form-group">
-                            <button class="btn btn-primary" type="button">
-                                Salvar</button>
-                        </div>
-                    </div>
-                </div>
+
             </form>
-        </div>
-
-
-
         <script>
-
               var aluno = document.getElementById('aluno');
               var curso = document.getElementById('curso');
               var bolsa = document.getElementById('bolsa');
@@ -211,10 +134,9 @@
               var filtrarAlunoId = document.getElementById('filtrarAluno');
 
               var alunosCursos = new Object();
-
-              curso.style.display = 'none';
-              bolsa.style.display = 'none';     
+              var listaCursos = @php echo json_encode($cursos); @endphp;
               var listaAlunos = @php echo json_encode($usuarios); @endphp;
+              
            
               function iniciarTabelaAlunos(){
                 for(let i = 0; i < listaAlunos.length; i++){
@@ -227,7 +149,7 @@
                 }
               }
 
-              window.onload = filtrarAlunoByName();  
+              iniciarTabelaAlunos();
 
               idAlunoFiltro.addEventListener('keyup', function(e){
                 var key = e.which || e.keyCode;
@@ -270,12 +192,63 @@
                   return tabelaAlunos.style.display = 'none';
               }
 
-              var avancarCurso   = () => {
+              function avancarCurso() {
+                  var divAluno = document.getElementById("escolhaAluno");
+                  divAluno.style.display = 'none';
                   progressbar.style.width = '33%';
+                  
+                  console.log("listadecursos:", listaCursos);
+
+                  var tabelaCursos = document.getElementById("tabelaCursos");
+                  //tabelaCursos.innerHTML = "";
+
+                  for(let i = 0; i < listaCursos.length; i++){
+                    tabelaCursos.innerHTML += `<tr> 
+                                                    <td scope="row">${listaCursos[i].id}</td>
+                                                    <td scope="row">${listaCursos[i].nome}</td>
+                                                    <td scope="row">${listaCursos[i].imagem}<td>
+                                                    <td scope="row">${listaCursos[i].conteudo}<td>
+                                                    <td scope="row"><button type="button" class="btn btn-success" onclick="selecionarCurso(${listaCursos[i].id})">Selecionar</button></td>
+                                                    </tr>` ;   
+                    console.log(listaCursos);
+                }
               }
 
-              var avancarBolsa = () => {}
+              function avançarCheckOut(){
+                var divCurso = document.getElementById("escolhaCurso");
+                divCurso.style.display = 'none';
+                progressbar.style.width = '100%';
 
+                document.getElementById("idAluno").value = alunosCursos.idAluno;
+                document.getElementById("nomeAluno").value = alunosCursos.aluno.nome;
+                document.getElementById("email").value = alunosCursos.aluno.email;
+                document.getElementById("cpf").value = alunosCursos.aluno.cpf;
+                document.getElementById("idCurso").value = alunosCursos.idCurso;
+                document.getElementById("nomeCurso").value = alunosCursos.nome;
+                document.getElementById("conteudo").value = alunosCursos.conteudo;
+              }
+
+              function selecionarCurso(id){
+                tabelaCursos.innerHTML = '';
+                for(let i = 0; i < listaCursos.length; i++){
+                    if(tabelaCursos[i].id == id){
+                        
+                        tabelaCursos.innerHTML += `<tr> 
+                                                    <td scope="row">${listaCursos[i].id}</td>
+                                                    <td scope="row">${listaCursos[i].nome}<td>
+                                                    <td scope="row">${listaCursos[i].conteudo}</td>
+                                                    <td scope="row"><button type="button" onclick="selecionadoReverter()" class="btn btn-danger">Deselecionar</button></td>
+                                                   </tr>` ;
+                                                                                   
+                        alunosCursos.idCurso = id;
+                        alunosCursos.curso.nome = listaCursos[i].nome;
+                        alunosCursos.curso.conteudo = listaCursos[i].conteudo;
+                        console.log("Objeto Aluno", alunosCursos);
+                        break;               
+                    }
+                }
+
+              }
               function selecionarAluno(id){
                 tabelaAlunos.innerHTML = '';
                 for(let i = 0; i < listaAlunos.length; i++){
@@ -289,6 +262,9 @@
                                                    </tr>` ;
                                                                                    
                         alunosCursos.idAluno = id;
+                        alunosCursos.aluno.nome = listaAlunos[i].nome;
+                        alunosCursos.aluno.email = listaAlunos[i].email;
+                        alunosCursos.aluno.cpf = listaAlunos[i].cpf;
                         console.log("Objeto Aluno", alunosCursos);
                         break;               
                     }
@@ -298,7 +274,7 @@
 
               function selecionadoReverter(){
 
-              }
+              }              
       </script>
     </body>
 </html>
